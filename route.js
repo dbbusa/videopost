5d03fa6ceea0e82e4bee70782c9fc7aa44b1de45
dbusa429 <darshitbusa8@gmail.com>
2021-02-04 18:05:35 +0530

    login api done

--- route.js
+++ route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(express.json());
 
-// For Users
+// For register User
 router.post("/register", async (req, res) => {
     var saltkey = await bcrypt.genSalt(10);
     var hasedPass = await bcrypt.hash(req.body.password, saltkey);
@@ -17,6 +17,21 @@ router.post("/register", async (req, res) => {
     });
     await user.save();
     res.send("Registerd Successfully");
+});
+
+// For login User
+router.post("/login", async (req, res) => {
+    const user = await User.findOne({ username: req.body.username});
+    if (!user)
+        return res.send("User Not Found..!!");
+    else{
+        const isValid = await bcrypt.compare(req.body.password,user.password);
+        if (!isValid)
+            return res.send("Invalid Password Try Again...")
+        else{
+            res.send("User Founded");
+        }
+    } 
 });
 
 
