cfad73e1e55f29efaca3dcc8a0e95809872651bb
Darshit Busa <darshitbusa8@gmail.com>
2021-04-14 19:46:29 +0530

    Video GET fix test

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ mongoose.connect("mongodb+srv://user1:apstndp16@cluster0.vqjzb.mongodb.net/stude
         app.use(express.json());
         // Add headers
         app.use(function (req, res, next) {
-            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
             res.setHeader('Access-Control-Allow-Origin', 'https://video-post.herokuapp.com');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
             res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
